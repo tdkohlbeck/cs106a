141fbbd385214d2763f0a403f5f55fbf1486060b
Travis Kohlbeck <tdk@apples-MacBook-Pro-6.local>
2016-02-27 15:59:34 -0800

    first completed assignment and solution

---
+++ handout-2/Fibonacci.java
@@ -0,0 +1,29 @@
+/*
+ * File: Fibonacci.java
+ * Name: Travis Kohlbeck
+ * -----------------------------
+ * This file prints the Fibonacci sequence up to MAX_TERM_VALUE
+ */
+
+import acm.program.*;
+
+public class Fibonacci extends ConsoleProgram {
+
+	private static final int MAX_TERM_VALUE = 10000;
+	
+	public int Fib(int n) {
+		switch (n) {
+			case 0: return 0;
+			case 1: return 1;
+		}
+		return Fib(n-1) + Fib(n-2);
+	}
+
+	public void run() {
+		println("This program lists the Fibonacci sequence.");
+		for (int i = 1, term = 0; term < MAX_TERM_VALUE; i++) {
+			println(term);
+			term = Fib(i);
+		}
+	}
+}
